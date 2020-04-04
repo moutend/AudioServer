8d5fe3478685c192a91fdbc7ce2c78a829ec11dd
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-04-04 09:21:18 +0900

    Add

---
+++ internal/app/app.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi"
+	"github.com/moutend/AudioServer/internal/api"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var RootCommand = &cobra.Command{
+	Use:  "todoapp",
+	RunE: rootRunE,
+}
+
+func rootRunE(cmd *cobra.Command, args []string) error {
+	if path, _ := cmd.Flags().GetString("config"); path != "" {
+		viper.SetConfigFile(path)
+	}
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+
+	router := api.Setup(chi.NewRouter())
+
+	address := "localhost:7901"
+
+	if a := viper.GetString("server.address"); a != "" {
+		address = a
+	}
+
+	cmd.Printf("Listening on %s\n", address)
+
+	return http.ListenAndServe(address, router)
+}
+
+func init() {
+	RootCommand.PersistentFlags().StringP("config", "c", "", "Path to configuration file")
+}
