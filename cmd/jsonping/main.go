85871a35ed72963f55992c851826a1011c050bf3
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-04-20 08:45:41 +0900

    Add

---
+++ cmd/jsonping/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("error: ")
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var (
+		pathFlag   string
+		methodFlag string
+	)
+
+	flag.StringVar(&pathFlag, "path", "", "Path to the JSON file.")
+	flag.StringVar(&methodFlag, "method", "", "HTTP method e.g. POST")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		return fmt.Errorf("specify address")
+	}
+
+	address := flag.Args()[0]
+
+	data, err := ioutil.ReadFile(pathFlag)
+
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(methodFlag, address, bytes.NewBuffer(data))
+
+	if err != nil {
+		return err
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	body := &bytes.Buffer{}
+
+	if _, err := io.Copy(body, res.Body); err != nil {
+		return err
+	}
+
+	fmt.Printf("%v %v\n", res.Status, body.Bytes())
+
+	return nil
+}
