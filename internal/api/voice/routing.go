9818fe3f60dd31e73d28e084dd326082e39a4f1f
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-05-19 08:30:03 +0900

    Update

--- internal/api/voice/routing.go
+++ internal/api/voice/routing.go
@@ -5,6 +5,17 @@ import "github.com/go-chi/chi"
 func Setup(router chi.Router) {
 	router.Route("/voice", func(r chi.Router) {
 		r.Get("/", getVoices)
+
+		r.Post("/voice/pitch/up", postDefaultVoicePitchUp)
+		r.Post("/voice/pitch/down", postDefaultVoicePitchDown)
+		r.Post("/voice/rate/up", postDefaultVoiceRateUp)
+		r.Post("/voice/rate/down", postDefaultVoiceRateDown)
+		r.Post("/voice/volume/up", postDefaultVoiceVolumeUp)
+		r.Post("/voice/volume/down", postDefaultVoiceVolumeDown)
+
+		r.Get("/", getDefaultVoiceProperty)
+		r.Put("/", putDefaultVoiceProperty)
+
 		r.Get("/{voiceId}", getVoiceProperty)
 		r.Put("/{voiceId}", putVoiceProperty)
 	})
